feed6afe558ee15acc3beed0c03677f25ec24956
phamlam2k <mailam1309@gmail.com>
2022-11-24 23:49:35 +0700

    Add api chat and Chat DB

---
+++ src/models/Message.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+
+export type MessageType = {
+  Message: { Text: string }
+  Users: unknown
+  Sender: mongoose.Schema.Types.ObjectId
+}
+
+const MessageSchema = new mongoose.Schema(
+  {
+    Message: {
+      Text: {
+        type: String,
+        required: true,
+      },
+    },
+    Users: Array,
+    Sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    collection: 'Message',
+    timestamps: true,
+  },
+)
+
+export const MessageModel = mongoose.model<MessageType>('Message', MessageSchema)
--- src/routes/private.router.ts
+++ src/routes/private.router.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import * as randomController from '../controllers/randomCard.controller'
 import * as postStatsController from '../controllers/stats.controller'
+import * as messageController from '../controllers/message.controller'
 
 const router = express.Router()
 
@@ -9,5 +10,9 @@ router.get('/api/random', randomController.randomCard)
 
 // Api get stats
 router.get('/api/post-stats', postStatsController.getPostStats)
+
+// Api message
+router.post('/api/message/send-msg', messageController.sendMsg)
+router.post('/api/message/received-msg', messageController.receivedMsg)
 
 export default router
